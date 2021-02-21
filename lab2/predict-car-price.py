09335de1ce252e5c3b583076cd873707e03670a4
huiyingli0125 <huiyingli2014@gmail.com>
2021-02-21 00:15:49 -0800

    lab2

--- lab2/predict-car-price.py
+++ lab2/predict-car-price.py
@@ -17,7 +17,73 @@ class CarPrice:
             self.df[col] = self.df[col].str.lower().str.replace(' ', '_')
 
     def validate(self):
-        pass
+        np.random.seed(2)
+
+        n = len(self.df)
+
+        n_val = int(0.2 * n)
+        n_test = int(0.2 * n)
+        n_train = n - (n_val + n_test)
+
+        idx = np.arange(n)
+        np.random.shuffle(idx)
+
+        df_shuffled = self.df.iloc[idx]
+
+        df_train = df_shuffled.iloc[:n_train].copy()
+        df_val = df_shuffled.iloc[n_train:n_train + n_val].copy()
+        df_test = df_shuffled.iloc[n_train+n_val:].copy()
+
+        y_train = df_train.msrp.values
+        y_val = df_val.msrp.values
+        y_test = df_test.msrp.values
+
+        return df_train, df_val, df_test, y_train, y_val, y_test 
 
     def linear_regression(self, X, y):
-        pass
+        ones = np.ones(X.shape[0])
+    
+        X = np.column_stack([ones, X])
+
+        XTX = X.T.dot(X)
+        XTX_inv = np.linalg.inv(XTX)
+        w = XTX_inv.dot(X.T).dot(y)
+        
+        return w[0], w[1:]
+    
+
+    def prepare_X(self, df):
+        base = ['engine_hp', 'engine_cylinders', 'highway_mpg', 'city_mpg', 'popularity']
+        df_num = df[base]
+        print (df_num)
+        df_num = df_num.fillna(0)
+        X = df_num.values
+        return X
+    
+    def predict(self, df, y):
+        X = self.prepare_X(df)
+        w_0, w = self.linear_regression(X, y)
+        y_pred = w_0 + X.dot(w)
+        return y_pred
+
+    def rmse(self, y, y_pred) -> float:
+        error = y_pred - y
+        mse = (error ** 2).mean()
+        return np.sqrt(mse)
+
+def test() -> None:
+    carPrice = CarPrice()
+    carPrice.trim()
+    df_train, df_val, df_test, y_train, y_val, y_test = carPrice.validate()
+    y_train_pred = carPrice.predict(df_train, y_train)
+    y_val_pred = carPrice.predict(df_val, y_val)
+    y_test_pred = carPrice.predict(df_test, y_test)
+    #train_rmse = carPrice.rmse(y_train, y_train_pred)
+    #val_rmse = carPrice.rmse(y_val, y_val_pred)
+    #test_rmse = carPrice.rmse(y_test, y_test_pred)
+    df_test['msrp_pred'] = y_test_pred
+    print (df_test.head(5))
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    test()
